93a035947ec96777b20f800026c01827b274fe33
Benjamin Lee <benjlee92@gmail.com>
2017-12-24 21:53:35 -0800

    Initial Commit

---
+++ AppyHour/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppyHour
+//
+//  Created by Ben Lee on 12/24/17.
+//  Copyright © 2017 Ben Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
